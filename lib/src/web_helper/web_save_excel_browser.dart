9510156355d15bf8db1e2d6a141637b3605d2633
tri_dev <phanminh.tri@quocviet.com.vn>
2025-07-24 16:34:49 +0700

    Fixed: downdload excel web

--- lib/src/web_helper/web_save_excel_browser.dart
+++ lib/src/web_helper/web_save_excel_browser.dart
@@ -1,26 +1,26 @@
 import 'dart:js_interop';
-import 'dart:typed_data';
-
 import 'package:web/web.dart';
 
 // A wrapper to save the excel file in browser
 class SavingHelper {
   static List<int>? saveFile(List<int>? val, String fileName) {
-    if (val == null) {
-      return null;
-    }
-
-    final blob = Blob(JSArray.from(Uint8List.fromList(val).toJS));
+    if (val == null) return null;
+    // Convert List<int> to JSArray<BlobPart>
+    final blobParts = ([val] as dynamic) as JSArray<BlobPart>;
+    final blob = Blob(
+        blobParts,
+        BlobPropertyBag(
+            type:
+                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'));
     final url = URL.createObjectURL(blob);
     final anchor = HTMLAnchorElement()
       ..href = url
-      ..download = '$fileName';
-
+      ..style.display = 'none'
+      ..download = fileName;
     document.body?.append(anchor);
 
     // download the file
     anchor.click();
-
     // cleanup
     anchor.remove();
     URL.revokeObjectURL(url);
